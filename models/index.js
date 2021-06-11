89b11695585955852d3b77d082a7eaf0adbbfeda
ahmed hakeem <hakeemkoo@hotmail.com>
2021-06-11 01:19:37 -0400

    fix the inxdex.js in the module file

--- models/index.js
+++ models/index.js
@@ -1,13 +1,29 @@
 const User = require('./User');
-const Bolg = require('./Bolg');
+const Post = require('./Post');
+const Comments = require('./Comments')
 
-User.hasMany(Bolg , {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-Project.belongsTo(User, {
+User.hasMany(Post, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Bolg };
+Post.hasMany(Comments, {
+  foreignKey: "post_id"
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comments.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Comments.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Comments, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comments };
